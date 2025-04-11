de378e7307ca2dbfcb151bf8b7e7b8f75be4ba58
Javier Alvarado <jdrummer1@outlook.com>
2025-04-11 01:27:42 -0700

    Add favicon files, robots.txt, and meta description to layout.liquid

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,4 +2,5 @@ export default function(eleventyConfig) {
 	// Order matters, put this at the top of your configuration file.
   eleventyConfig.setInputDirectory("views");
   eleventyConfig.addPassthroughCopy("views/assets");
+  eleventyConfig.addPassthroughCopy({ "views/_meta": "/" });
 };
